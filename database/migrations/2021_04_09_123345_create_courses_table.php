7058f9c071142d40a1bdd9ae181af941f080c25b
Youta Obby Sidane <dyosby237@gmail.com>
2021-07-13 16:23:20 +0100

    Update course stable

--- database/migrations/2021_04_09_123345_create_courses_table.php
+++ database/migrations/2021_04_09_123345_create_courses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCourseTable extends Migration
+class CreateCoursesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateCourseTable extends Migration
      */
     public function up()
     {
-        Schema::create('course', function (Blueprint $table) {
+        Schema::create('courses', function (Blueprint $table) {
             $table->id();
             $table->string('intitule', 50);
             $table->foreignId('user_id')->nullable();
@@ -31,6 +31,6 @@ class CreateCourseTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('cours');
+        Schema::dropIfExists('courses');
     }
 }
